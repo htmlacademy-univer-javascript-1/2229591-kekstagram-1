ef43c94fb955471fddb27a003ab888dff41067fb
dmaterialist <turbocat14@gmail.com>
2022-09-24 02:18:35 +0300

    task 2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-let randomFromTo = function (from,to) {
-  if (to<0 || from<0 || to<from)
-    return -1
-  return Math.floor(from + Math.random() * (to + 1 - from))
-}
+const randomFromTo = function (from, to) {
+  if (to < 0 || from < 0 || to < from) {
+    return -1;
+  }
+  return Math.floor(from + Math.random() * (to + 1 - from));
+};
 
-let checkStringSize = (string,max) => string.length <=max
+const checkStringSize = (string, max) => string.length <= max;
