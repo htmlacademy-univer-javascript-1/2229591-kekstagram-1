659c0e46cbb268d0af4b45b405a6b7ed6a7f66ca
dmaterialist <turbocat14@gmail.com>
2022-11-22 01:47:45 +0300

    task 8_2

--- js/window.js
+++ js/window.js
@@ -1,27 +1,38 @@
 const image = document.querySelector('.big-picture');
 const close = image.querySelector('.big-picture__cancel');
-const commentCounter = image.querySelector('.social__comment-count');
+const commentCounter = image.querySelector('.comments-loaded');
 const commentsLoader = image.querySelector('.comments-loader');
 const commentsWindow = image.querySelector('.social__comments');
-
-const drawComments = (comments) => {
-  comments.forEach((comment) => {
-    const p = document.createElement('p');
-    p.classList.add('social__text');
-    p.textContent = comment.message;
-    const img = document.createElement('img');
-    img.classList.add('social__picture');
-    img.src = comment.avatar;
-    img.alt = comment.name;
-    img.width = 35;
-    img.height = 35;
-    const commentFrame = document.createElement('li');
-    commentFrame.classList.add('social__comment');
-    commentFrame.appendChild(img);
-    commentFrame.appendChild(p);
-    commentsWindow.appendChild(commentFrame);
+let comments;
+let loaded = 0;
+const drawComment = (comment) => {
+  const p = document.createElement('p');
+  p.classList.add('social__text');
+  p.textContent = comment.message;
+  const img = document.createElement('img');
+  img.classList.add('social__picture');
+  img.src = comment.avatar;
+  img.alt = comment.name;
+  img.width = 35;
+  img.height = 35;
+  const commentFrame = document.createElement('li');
+  commentFrame.classList.add('social__comment');
+  commentFrame.appendChild(img);
+  commentFrame.appendChild(p);
+  commentsWindow.appendChild(commentFrame);
+};
+const drawComments = () => {
+  if (comments.length-loaded<=5) {
+    const commentsCopy = comments.slice(loaded, comments.length);
+    commentsCopy.forEach((comment) => drawComment(comment));
+    commentsLoader.classList.add('hidden');
+    loaded=comments.length;
+  } else {
+    const commentsCopy = comments.slice(loaded, loaded+5);
+    commentsCopy.forEach((comment) => drawComment(comment));
+    loaded += 5;
   }
-  );
+  commentCounter.textContent = loaded;
 };
 
 export const hide = (element) => {
@@ -34,24 +45,30 @@ export const show = (element) => {
   element.classList.remove('hidden');
 };
 
+const exit = () => {
+  hide(image);
+  commentsLoader.removeEventListener('click',drawComments);
+};
+
 export const drawWindow = (picture) => {
+  loaded=0;
   show(image);
-  commentCounter.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
-  close.addEventListener('click', () => hide(image));
+  close.addEventListener('click', exit);
   document.addEventListener('keydown',(evt) => {
     if (evt.key === 'Escape') {
-      hide(image);
+      exit();
     }
   });
   image.querySelector('.big-picture__img').querySelector('img').src = picture.url;
   image.querySelector('.likes-count').textContent = picture.likes;
   image.querySelector('.social__caption').textContent = picture.description;
-  const comments = picture.comments;
-  image.querySelector('.comments-count').textContent = comments.length.toString();
+  comments = picture.comments;
+  image.querySelector('.comments-count').textContent = comments.length;
   const oldComments = image.querySelectorAll('.social__comment');
+  commentsLoader.classList.remove('hidden');
   oldComments.forEach((comment)=>
     comment.remove());
-  drawComments(comments);
+  drawComments();
+  commentsLoader.addEventListener('click',drawComments);
 };
 
